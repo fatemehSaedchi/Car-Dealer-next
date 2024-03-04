27d63564115d0ea32e95d281d9f2caa92bbff6fc
Fatemeh <fatemeh.saedchi@gmail.com>
2024-03-04 12:53:09 +0330

    implement Menu Mock

---
+++ src/mock/menu.ts
@@ -0,0 +1,31 @@
+export const menuMock = [
+    {
+        title: 'Home',
+        link: "#"
+    },
+
+    {
+        title: 'About',
+        link: "#"
+    },
+
+    {
+        title: 'Service',
+        link: "#"
+    },
+
+    {
+        title: 'Car Collection',
+        link: "#"
+    },
+
+    {
+        title: 'Location',
+        link: "#"
+    },
+
+    {
+        title: 'Contact',
+        link: "#"
+    },
+]
