5bc636055d6db40b8e77bc21655096e0c8864861
Fatemeh <fatemeh.saedchi@gmail.com>
2024-03-04 13:04:08 +0330

    edit menu components

--- src/components/layouts/header/menu/Menu.tsx
+++ src/components/layouts/header/menu/Menu.tsx
@@ -1,11 +1,7 @@
 import Link from "next/link";
 import {menuMock} from "@/mock/menu";
 
-interface Props {
-    
-}
-
-export function Menu({}: Props) {
+export function Menu() {
     return (
         <ul id="navList" className="absolute bg-White-200 text-secondary-300 font-semibold z-[9999] left-0 top-0 w-full h-screen py-5 px-4 -translate-x-full sm:w-8/12 lg:translate-x-0 lg:relative lg:flex lg:bg-transparent lg:w-fit lg:gap-4 lg:p-0 lg:h-fit 2xl:gap-7">
             <li className="lg:hidden">
