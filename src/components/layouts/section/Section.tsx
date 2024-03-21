5e4cd52967c71e9e1f9beb973a8d3d2e5b1e8b5c
mehrshadiy <mehrshadshakery@yahoo.com>
2024-03-21 22:03:00 +0330

    fix Section.tsx component style

--- src/components/layouts/section/Section.tsx
+++ src/components/layouts/section/Section.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export function Section({className='', children}: Props) {
     return (
-        <section className={`container mx-auto px-4 mb-28 lg:mb-52 ${className}`}>
+        <section className={`container mx-auto px-4 mb-28 relative lg:mb-52 ${className}`}>
             {children}
         </section>
     )
