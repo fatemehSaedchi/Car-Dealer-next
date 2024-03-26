92e304acdf4e4cfeef9381e7b100028b84869d52
Fatemeh <fatemeh.saedchi@gmail.com>
2024-03-26 14:57:59 +0430

    add Map component to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,4 @@
-import {HeroSection, IconBox, OurTeam, Section} from "@/components";
+import {HeroSection, IconBox, OurTeam, Section, Map} from "@/components";
 import {TeamMembersMock} from "@/mock";
 
 interface Props {
@@ -90,42 +90,7 @@ export default function About({}: Props) {
                 </div>
             </Section>
 
-
-            {/* //todo replace section with ms.saedchi's component
-                <section>
-                <div className="container mx-auto px-4 pt-16 lg:pt-24">
-                    <h3 className="font-bold text-xl md:text-2xl lg:text-[35px] xl:text-[45px] text-center">Our dealer
-                        shops</h3>
-                    <p className="sm:w-4/6 max-w-2xl m-auto pt-2 lg:pt-5 text-center text-xs md:text-sm xl:text-base leading-5">
-                        Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.</p>
-                    <div className=" mt-5">
-                        <div className="max-w-6xl mx-auto relative">
-                            <img src="../assets/img/about/map.svg" alt=""/>
-                            <div
-                                className="w-[10%] h-1/6 max-w-[80px] max-h-[77px] absolute top-[15%] left-[8%] rounded-xl bg-white flex justify-center items-center shadow-xl">
-                                <img className="w-[50%]" src="../assets/img/about/car-blue.png" alt=""/>
-                            </div>
-                            <div
-                                className="w-[13%] h-[21%] max-w-[95px] max-h-[90px] absolute top-[35%] left-[40%] rounded-2xl  bg-primary-100 flex justify-center items-center">
-                                <img className="w-[50%]" src="../assets/img/about/carVector.png" alt=""/>
-                            </div>
-                            <div
-                                className="w-[15%] h-1/4 max-w-[110px] max-h-[108px] absolute top-[18%] right-[10%] rounded-2xl bg-white flex justify-center items-center shadow-xl">
-                                <img className="w-[50%]" src="../assets/img/about/car-blue.png" alt=""/>
-                            </div>
-                            <div
-                                className="w-[8%] h-[13%] max-w-[58px] max-h-[56px] absolute bottom-[20%] left-1/2 rounded-lg bg-white flex justify-center items-center shadow-xl">
-                                <img className="w-[50%]" src="../assets/img/about/car-blue.png" alt=""/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            */}
-
+            <Map/>
 
             <Section>
                 <div className="flex flex-col sm:flex-row gap-3 sm:gap-24 lg:gap-48">
