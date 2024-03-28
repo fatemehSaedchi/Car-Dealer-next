548655faa5d97f4b791c8cd8d4b8610c40487a59
mehrshadiy <mehrshadshakery@yahoo.com>
2024-03-28 23:36:19 +0330

    do todo of OurTeam.tsx component

--- src/components/pages/aboutPage/ourTeam/OurTeam.tsx
+++ src/components/pages/aboutPage/ourTeam/OurTeam.tsx
@@ -1,6 +1,5 @@
-// todo creat card
 
-import {ImageView} from "@/components";
+import {TeamCard} from "@/components";
 
 interface Props {
     data: Array<any>
@@ -13,29 +12,9 @@ export function OurTeam({data}: Props) {
 
             <div className="md:w-5/6 mx-auto grid grid-cols-3 sm:grid-cols-4 gap-3 lg:gap-x-9 gap-y-9 pt-16 lg:pt-24 ">
                 {
-                    data.map((item, index) => {
+                    data.map((value, index) => {
                         return (
-                            <div className={`h-fit px-1 lg:px-3 2xl:px-6 relative ${index === data.length - 1 ? "col-end-4 sm:col-end-5" : ""}`}>
-
-                                {
-                                    index === 0 &&
-                                    <ImageView height={66} width={50} src="/assets/images/circleElement.svg" alt=""
-                                               classname="w-10 lg:w-16 absolute -left-2.5 -top-3 lg:-top-5"/>
-                                }
-
-                                {
-                                    index === (data.length - 1) &&
-                                    <ImageView height={66} width={50} src="/assets/images/circleElement.svg" alt=""
-                                               classname="w-10 lg:w-16 absolute -left-2.5 bottom-16 lg:bottom-20"/>
-                                }
-
-                                <ImageView height={640} width={931} src={item.thumbNail}
-                                           alt=""
-                                           classname="w-full max-h-72 object-cover object-center rounded-xl"/>
-                                <h4 className="text-center font-bold text-xs lg:text-base pt-4">
-                                    {item.title}
-                                </h4>
-                            </div>
+                            <TeamCard data={value} key={index} first={index === 0 } last={index === (data.length - 1)}/>
                         )
                     })
                 }
