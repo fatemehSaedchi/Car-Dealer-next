2e3768ba2a05224e1a8d00221f50b2cb85f28a70
mehrshadiy <mehrshadshakery@yahoo.com>
2024-03-28 23:35:20 +0330

    do todo of PaginatedSlider.tsx component

--- src/components/common/ui/sliders/PaginatedSlider.tsx
+++ src/components/common/ui/sliders/PaginatedSlider.tsx
@@ -1,8 +1,6 @@
-// todo creat card component
-
-import {CarFeatures, ImageView} from "@/components";
+import {PaginatedSliderCard} from "@/components";
 import {Swiper, SwiperSlide} from "swiper/react";
-import {Pagination} from "swiper/modules";
+import {Autoplay, Pagination} from "swiper/modules";
 
 
 interface Props {
@@ -16,12 +14,13 @@ export function PaginatedSlider({sliderData}: Props) {
         <>
 
             <Swiper
-                modules={[Pagination]}
+                modules={[Pagination,Autoplay]}
                 className="mySwiper rounded-[30px]"
                 pagination={{
                     clickable: true
                 }}
                 slidesPerView={1}
+                autoplay={true}
                 breakpoints={{
                     640: {
                         slidesPerView: 2,
@@ -34,26 +33,11 @@ export function PaginatedSlider({sliderData}: Props) {
                 }}
             >
 
-                {sliderData.map((item, index: number)=>{
+                {sliderData.map((value, index: number)=>{
                     return(
                         <>
-                            <SwiperSlide key={index} className={'relative rounded-[30px] overflow-hidden'}>
-                            <ImageView height={1920} width={2880}
-                                       classname="rounded-[30px] h-[435px] xl:h-[495px]"
-                                       src={item.thumbnail} alt=""/>
-                            <div className="absolute top-8 md:top-9 xl:top-11 left-6 xl:left-10 text-white flex flex-col items-start">
-                                <p className="text-xs xl:text-sm font-medium tracking-[4px]">
-                                    {item.car_type} CAR
-                                </p>
-                                <p className="text-2xl xl:text-3xl font-bold pt-40 xl:pt-44">
-                                    {item.title}
-                                </p>
-                                <button
-                                    className="px-4 py-2 bg-primary-100 rounded-[30px] text-base xl:text-lg font-semibold mt-8">
-                                    $ {item.price}
-                                </button>
-                                <CarFeatures item={item}/>
-                            </div>
+                        <SwiperSlide key={index} className={'relative rounded-[30px] overflow-hidden'}>
+                        <PaginatedSliderCard key={index} data={value}/>
                         </SwiperSlide>
                         </>
                     )
