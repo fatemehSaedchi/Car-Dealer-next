da0ba27a27981654d5664de5b6febb68e1bb2bc6
Fatemeh <fatemeh.saedchi@gmail.com>
2024-04-15 14:56:05 +0430

    edit fuel and transmission type

--- src/types/FuelAndTransmission.ts
+++ src/types/FuelAndTransmission.ts
@@ -1,9 +1,9 @@
 import {CarsType} from "@/types/Cars";
-import {EntityType} from "@/types/Response";
+import {PopulateType} from "@/types/Response";
 
 export interface FuelTransitionType {
     type: string
     createdAt: string
     updatedAt: string
-    cars: EntityType<CarsType>
+    cars: PopulateType<CarsType>
 }
